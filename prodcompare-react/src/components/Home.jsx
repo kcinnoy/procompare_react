3a35c116cc71b77626d2f8ac2647c7dd1f09ddda
kcinnoy <yonnick00@gmail.com>
2019-10-20 23:09:29 +0100

    basic component structure

---
+++ prodcompare-react/src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React, {useState, useEffect} from 'react';
+
+import ProductApp from "../chartComponent/src/ProductApp";
+
+const Home = () => {
+    return (
+       <ProductApp />
+    )
+};
+
+export default Home;
