b439d2f6e60c6a869badb9fe492c60efe6e83e3f
kcinnoy <yonnick00@gmail.com>
2019-11-03 14:21:21 +0000

    added sign up

---
+++ prodcompare-react/src/actions/signup.js
@@ -0,0 +1,21 @@
+import Api from "../utils/api";
+
+export const signupAction = (email, password, handleResponse, handleError) => {
+    return dispatch => {
+        Api.post('/users.json', {
+                email: email,
+                password: password
+            }, {
+                withCredentials: true,
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+            }
+        ).then(response => {
+            handleResponse(response);
+        }).catch(error => {
+            handleError(error)
+        });
+    };
+};
